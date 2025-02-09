5ba41e30511a7f0c0774bf19638d02cf6dbf1570
Sahil0282 <xyz3474abc@gmail.com>
2025-02-09 11:32:02 +0530

    feat(ML):Langflow 3 APIs running

--- MyWellness_backend/api/urls.py
+++ MyWellness_backend/api/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import SignupView, SigninView
+from .views import SignupView, SigninView,chatbot,physical_activity_recommendation,sleep_recommendation
 
 urlpatterns = [
     path('register/', SignupView.as_view(), name='signup'),
     path('signin/', SigninView.as_view(), name='signin'),
+    path('chatbot/', chatbot, name='chatbot'),
+    path('physical-activity-recommendation/', physical_activity_recommendation, name='physical_activity_recommendation'),
+    path('sleep-recommendation/', sleep_recommendation, name='sleep_recommendation'),
 ]
